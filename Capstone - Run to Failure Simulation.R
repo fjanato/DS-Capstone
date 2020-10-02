3e0bf982896792a76945d400004e1e3ad2e7d027
Freddy Anato <fanato@freddys-imac.attlocal.net>
2020-10-02 00:21:27 -0500

    Complete Regression Models

--- Capstone - Run to Failure Simulation.R
+++ Capstone - Run to Failure Simulation.R
@@ -69,13 +69,14 @@ summary(train)
 
 
 # Find the last Cycle number for each UnitNum (Jet Engine) in the Train set
-TrainFailCycle <- tapply(train$TimeCycles, train$UnitNum, max)
+TrainFailCycle <- as.vector(tapply(train$TimeCycles, train$UnitNum, max))
 
 # Find the last Cycle number for each UnitNum (Jet Engine) in the Test set
-TestFailCycle <- tapply(test$TimeCycles, test$UnitNum, max)
+TestFailCycle <- as.vector(tapply(test$TimeCycles, test$UnitNum, max))
 
 # Plot Last Cycle per Unit
 qplot(y= TrainFailCycle, x=1:length(TrainFailCycle), main = 'Failure Cycle by Engine', ylab = 'Cycle of Failure', xlab = 'Unit Number', geom = "line")
+
 
 # Show distribution of Failure Cycle in Engines
 hist(TrainFailCycle)
@@ -89,7 +90,25 @@ RealCTF <- unlist(CTF)
 train$CyclesToFail <- TrainFailCycle[train$UnitNum] - train$TimeCycles
 
 # Calculate Cycles to Fails Value for Test Set
-test$CyclesToFail <-   RealCTF[test$UnitNum] + TestFailCycle[test$UnitNum] - test$TimeCycles
+test$CyclesToFail <-   RealCTF[test$UnitNum] + TestFailCycle[test$UnitNum]
+
+test$CyclesToFail <-  test$CyclesToFail - test$TimeCycles
+
+
+#Plot RUL per cycle number on Train set
+ggplot(train, aes(TimeCycles, CyclesToFail) ) +
+  geom_point() +
+  stat_smooth() +
+  ggtitle("Cycles Runs per Engine vs Cycles to Fail")
+
+
+#Plot RUL per cycle number on Test set
+ggplot(test, aes(TimeCycles, CyclesToFail) ) +
+  geom_point() +
+  stat_smooth() +
+  
+
+
 
 # Build the preliminary list of variables for the prediction model 
 num_cols = c('OperSet1','OperSet2','SensorRead02','SensorRead03','SensorRead04','SensorRead06','SensorRead07','SensorRead08','SensorRead09',
@@ -141,21 +160,19 @@ head(train[,rev_cols])
 
 
 #TrainControl <- trainControl(method = "repeatedcv", number = 10, repeats = 10, verboseIter=FALSE)  
-TrainFormula <- CyclesToFail ~ SensorRead02 + SensorRead03 + SensorRead04 + SensorRead07 +
-  SensorRead11 + SensorRead12 + SensorRead15 + SensorRead17 + SensorRead20 + SensorRead21
+TrainFormula <- CyclesToFail ~ SensorRead02 + SensorRead03 + SensorRead04 + SensorRead07 + SensorRead08 + SensorRead09 +
+  SensorRead11 + SensorRead12 + SensorRead13 + SensorRead14 + SensorRead15 + SensorRead17 + SensorRead20 + SensorRead21
 
-TrainFormula <- CyclesToFail ~ SensorRead09 + SensorRead14 
 
 #########################
 #apply linear regression
 ## define and fit the linear regression model
+
+
 lin_mod = lm(TrainFormula, data = train)
 
-summary(lin_mod)$coefficients
-
-
 print_metrics = function(lin_mod, df, score, label){
-  resids = df[,label] - score
+  resids = df[,label] - df[,score]
   resids2 = resids**2
   N = length(score)
   r2 = as.character(round(summary(lin_mod)$r.squared, 4))
@@ -163,14 +180,131 @@ print_metrics = function(lin_mod, df, score, label){
   cat(paste('Mean Square Error      = ', as.character(round(sum(resids2)/N, 4)), '\n'))
   cat(paste('Root Mean Square Error = ', as.character(round(sqrt(sum(resids2)/N), 4)), '\n'))
   cat(paste('Mean Absolute Error    = ', as.character(round(sum(abs(resids))/N, 4)), '\n'))
-  #cat(paste('Median Absolute Error  = ', as.character(round(median(abs(resids)), 4)), '\n'))
+  cat(paste('Median Absolute Error  = ', as.character(round(median(abs(unlist(resids))), 4)), '\n'))
   cat(paste('R^2                    = ', r2, '\n'))
   cat(paste('Adjusted R^2           = ', adj_r2, '\n'))
 }
 
 #score = predict(lin_mod, newdata = test)
-test$score <- predict(lin_mod, newdata = test)
-print_metrics(lin_mod, test, score, label = 'CyclesToFail')  
+test$LR_score <- predict(lin_mod, newdata = test)
+
+
+# Model performance
+predictions <- lin_mod %>% predict(test)
+# data.frame("Linear Regression Model",
+#   RMSE = RMSE(predictions, test$CyclesToFail),
+#   R2 = R2(predictions, test$CyclesToFail)
+# )
+print_metrics(lin_mod, test, 'LR_score', 'CyclesToFail')  
+
+# Visualize Linear Regression Model
+
+ggplot(test, aes(CyclesToFail, LR_score) ) +
+  geom_point() +
+  stat_smooth(method = lm, formula = y ~ x)
+
+
+
+#############################
+## Apply Polynomial Regression
+
+
+# lm(CyclesToFail ~ polym(SensorRead02, SensorRead03, SensorRead04, SensorRead07, SensorRead08, SensorRead09,
+#                         SensorRead11, SensorRead12, SensorRead13, SensorRead14,
+#                         SensorRead15, SensorRead17, SensorRead20, SensorRead21, 
+#                          degree = 2, raw = TRUE), data = train) %>% summary()
+
+
+# Build the model
+poly_model <- lm(CyclesToFail ~ polym(SensorRead02, SensorRead03, SensorRead04, SensorRead07, SensorRead08, SensorRead09,
+                                      SensorRead11, SensorRead12, SensorRead13, SensorRead14,
+                                      SensorRead15, SensorRead17, SensorRead20, SensorRead21, 
+                                      degree = 2, raw = TRUE), data = train)
+
+
+# Make predictions
+test$poly_score <- predict(poly_model, newdata = test)
+predictions <- poly_model %>% predict(test)
+# Model performance
+print_metrics(poly_model, test, 'poly_score', 'CyclesToFail')  
+
+# Plot Model Accuracy
+ggplot(test, aes(CyclesToFail, poly_score) ) +
+  geom_point() +
+  stat_smooth(method = lm, formula = y ~ poly(x, 5, raw = TRUE))
+
+
+######################################
+#apply Random Forest
+rf_model = randomForest(TrainFormula, data = train, ntree = 5)
+
+test$RFscore = predict(rf_model, newdata = test)
+
+# Model performance
+print_metrics(rf_model, test, 'RFscore', 'CyclesToFail')  
+
+predictions <- rf_model %>% predict(test)
+# Model performance
+data.frame(
+  RMSE = RMSE(predictions, test$CyclesToFail),
+  R2 = 1-R2(predictions, test$CyclesToFail)
+)
+
+# Plot Model Accuracy
+ggplot(test, aes(CyclesToFail, RFscore) ) +
+  geom_point() +
+  stat_smooth(method = lm, formula = y ~ poly(x, 5, raw = TRUE))
+
+
+###############3
+
+
+
+
+
+#Print confussion Matrix to review performance
+print_metrics = function(df, label){
+  ## Compute and print the confusion matrix
+  cm = as.matrix(table(Actual = test$CyclesToFail, Predicted = test$RFscore))
+  print(cm)
+  
+  ## Compute and print accuracy
+  accuracy = round(sum(sapply(1:nrow(cm), function(i) cm[i,i]))/sum(cm), 3)
+  cat("\n")
+  cat(paste("Accuracy = ", as.character(accuracy)), "\n \n")
+
+  # Compute and print precision, recall and F1
+  precision = sapply(1:nrow(cm), function(i) cm[i,i]/sum(cm[i,]))
+  recall = sapply(1:nrow(cm), function(i) cm[i,i]/sum(cm[,i]))
+  F1 = sapply(1:nrow(cm), function(i) 2*(recall[i] * precision[i])/(recall[i] + precision[i]))
+
+  metrics = sapply(c(precision, recall, F1), round, 3)
+  metrics = t(matrix(metrics, nrow = nrow(cm), ncol = ncol(cm)))
+  metrics = t(matrix(metrics, nrow = nrow(cm), ncol = 3))
+  dimnames(metrics) = list(c("Precision", "Recall", "F1"), unique(test$CyclesToFail))
+  print(metrics)
+}  
+print_metrics(test, "CyclesToFail")
+
+
+
+
+
+
+options(repr.plot.width=4, repr.plot.height=3)
+imp = varImp(rf_model)
+imp = varImp(lin_mod)
+imp = varImp(poly_model)
+
+
+
+imp[,'Feature'] = row.names(imp)
+ggplot(imp, aes(x = Feature, y = Overall)) + geom_point(size = 4) +
+  ggtitle('Variable importance for features') +
+  theme(axis.text.x = element_text(angle = 45))
+
+options(repr.plot.width=8, repr.plot.height=6)
+print(imp)
 
 
 
@@ -184,95 +318,7 @@ hist_resids = function(df, score, label, bins = 10){
     xlab('Residual value') + ggtitle('Histogram of residuals')
 }
 
-hist_resids(test, score, label = 'CyclesToFail') 
+hist_resids(test, 'LR_score', 'CyclesToFail') 
 ######################
-
-
-
-#########
-#apply Random Forest
-rf_model = randomForest(TrainFormula, data = train, ntree = 5)
-
-test$scores = predict(rf_model, newdata = test)
-head(test[,c(2,28)])
-###############3
-
-
-
-
-
-
-
-
-
-#Print confussion Matrix to review performance
-print_metrics = function(df, label){
-  ## Compute and print the confusion matrix
-  cm = as.matrix(table(Actual = test$CyclesToFail, Predicted = test$scores))
-  print(cm)
-  
-  ## Compute and print accuracy 
-  #accuracy = round(sum(sapply(1:nrow(cm), function(i) cm[i,i]))/sum(cm), 3)
-  #cat("\n")
-  #cat(paste("Accuracy = ", as.character(accuracy)), "\n \n")                           
-  
-  ## Compute and print precision, recall and F1
-  #precision = sapply(1:nrow(cm), function(i) cm[i,i]/sum(cm[i,]))
- # recall = sapply(1:nrow(cm), function(i) cm[i,i]/sum(cm[,i]))    
-  #F1 = sapply(1:nrow(cm), function(i) 2*(recall[i] * precision[i])/(recall[i] + precision[i]))    
-  
-  #metrics = sapply(c(precision, recall, F1), round, 3)        
-  #metrics = t(matrix(metrics, nrow = nrow(cm), ncol = ncol(cm)))      
-  #metrics = t(matrix(metrics, nrow = nrow(cm), ncol = 3))       
-  #dimnames(metrics) = list(c("Precision", "Recall", "F1"), unique(test$CyclesToFail))      
-  #print(metrics)
-}  
-print_metrics(test, "CyclesToFail")
-
-
-
-
-
-
-
-options(repr.plot.width=4, repr.plot.height=3)
-#imp = varImp(rf_model)
-imp = varImp(lin_mod)
-
-
-
-imp[,'Feature'] = row.names(imp)
-ggplot(imp, aes(x = Feature, y = Overall)) + geom_point(size = 4) +
-  ggtitle('Variable importance for features') +
-  theme(axis.text.x = element_text(angle = 45))
-
-options(repr.plot.width=8, repr.plot.height=6)
-print(imp)
-
-# Remove less important Variables
-TrainFormulaRev <- TimeCycles ~ Operation_Setting_01 +  
-  Operation_Setting_02 + 
-  #Operation_Setting_03 + 
-  #Sensor_Measure_01 + 
-  Sensor_Measure_02 + 
-  Sensor_Measure_03 + 
-  Sensor_Measure_04 + 
-  #Sensor_Measure_05 + 
-  #Sensor_Measure_06 + 
-  Sensor_Measure_07 + 
-  Sensor_Measure_08 + 
-  Sensor_Measure_09 + 
-  #Sensor_Measure_10 + 
-  Sensor_Measure_11 + 
-  Sensor_Measure_12 + 
-  Sensor_Measure_13 + 
-  Sensor_Measure_14 + 
-  Sensor_Measure_15 + 
-  #Sensor_Measure_16 + 
-  Sensor_Measure_17 + 
-  #Sensor_Measure_18 + 
-  #Sensor_Measure_19 + 
-  Sensor_Measure_20 + 
-  Sensor_Measure_21
 
 
